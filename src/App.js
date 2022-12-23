dea6acbe3ef08f8e3f10ef8bf1a5d04e0ff0d0fb
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2022-12-23 16:37:41 -0500

    navbar component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     <Routes>
       <Route path='/' element={<Navbar />}>
         <Route 
-          path='/'
           index
           element={<Home />}
         />
--- src/routes/navbar/navbar.component.jsx
+++ src/routes/navbar/navbar.component.jsx
@@ -1,10 +1,14 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
 
 const Navbar = () => {
      return (
           <>
                <nav>
                     <h1>Navbar</h1>
+                    <Link to='/'>Home</Link>
+                    <Link to='/myfoods'>My Foods</Link>
+                    <Link to='/addfood'>Add Food</Link>
+                    <Link to='/auth'>Sign-in</Link>
                </nav>
                <Outlet />
           </>
